8d69dbc2a0b7d581adf36e87ecad701b5980e7d3
先生 <ateacher@iMac202001noiMac.local>
2020-12-10 16:47:33 +0900

    iOSでのPOSTリクエスト方法サンプルを追加

--- iOSSampleCode/ViewController.swift
+++ iOSSampleCode/ViewController.swift
@@ -59,5 +59,50 @@ class ViewController: UIViewController {
         // 作成したタスクを実行
         task.resume()
     }
+    
+    // POSTのサンプル
+    @IBAction func clickPostRequestWebAPI(_ sender: UIButton) {
+        // 接続先のURL文字列
+        let urlString = "http://172.16.13.21/khanh/sample/postsample.php"
+
+        // アクセスしたい先のURL情報をURL文字列から作成する
+        // urlはNSURL型
+        guard let url = URLComponents(string: urlString) else { return }
+        // POSTリクエストを作成する
+        var request = URLRequest(url: url.url!)
+        request.httpMethod = "POST"
+
+        let id = "iOStest"
+        let name = "abc"
+        let lang = "日本語"
+        
+        // サーバーに送信するリクエストへ、入れておくデータを文字列で作る
+        let bodyString : String = "id=\(id)"
+            + "&" + "name=\(name)"
+            + "&" + "language=\(lang)"
+        // 作ったデータをリクエストの中に入れられる形式に変換
+        let bodyData : Data = bodyString.data(using: .utf8)!
+        // POSTするデータをBodyとして設定
+        request.httpBody = bodyData
+        // HTTPメソッドを実行するタスクを作成（画面描画や操作とは別のスレッドで動く）
+        let task = URLSession.shared.dataTask(with: request)
+        {
+            (data, response, error) in
+            if (error != nil) {
+                print(error!.localizedDescription)
+            }
+            guard let _data = data else { return }
+
+            // 画面描画の更新（画面描画用のスレッド(mainスレッド)上で動かす必要がある）
+            DispatchQueue.main.sync {
+                self.resultDispLabel.text = String(data: _data, encoding: .utf8)
+            }
+            
+        }
+        // 作成したタスクを実行
+        task.resume()
+
+    }
+    
 }
 
